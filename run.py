029908b76dfade5002e66f111e1d7b582a764267
Roberto Arenal <beto.arenal@gmail.com>
2020-12-30 10:51:03 +0100

    commit prueba heroku

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 if __name__ == "__main__":
 
-    st.title('AJUA!') 
-    st.write('This app is the final project of DFS class')
+    st.title('AJUAAA!') 
+    st.write('This app is the final project of DSF class')
